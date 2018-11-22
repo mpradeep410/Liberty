d526788e75dd80e09f0a210fab586ed0e8d84c65
Pradeep <mpradeep410@gmail.com>
2018-11-22 19:49:03 +0530

    test

---
+++ LT/src/test/java/LibertyTa/LT/abc.java
@@ -0,0 +1,13 @@
+package LibertyTa.LT;
+
+import org.testng.annotations.Test;
+
+public class abc {
+	
+	@Test
+	public void testadfa()
+	{
+		System.out.println("TEst *****   *              ************** ");
+	}
+
+}
